893a92ddf249c5195decfda75543179cf0bdc1fa
MaxSeiringer <seiringer.maximilian@hak-liezen.at>
2024-01-12 15:20:35 +0100

    a

--- index.mjs
+++ index.mjs
@@ -1,23 +1,13 @@
 import { app } from './app.mjs';
-
-const PORT = process.env.PORT || 3000;
-
-//app.listen(PORT, () => {
-//  console.log(`Server läuft auf http://localhost:${PORT}`);
-//});
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server läuft auf http://0.0.0.0:${PORT}`);
-});
-
-
 import authrouter from './routes/authRoutes.mjs';
-app.use('/auth', authrouter);
-
 import uidrouter from './routes/uidRoutes.mjs';
-app.use('/uid', uidrouter);
-
 import nutritionrouter from './routes/nutritionRoutes.mjs';
-app.use('/nutrition', nutritionrouter);
-
 import trainingrouter from './routes/trainingRoutes.mjs';
+
+app.use('/auth', authrouter);
+app.use('/uid', uidrouter);
+app.use('/nutrition', nutritionrouter);
 app.use('/training', trainingrouter);
+
+export default app;
+
