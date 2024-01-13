61d51b4791786bfadd753d1e15b06a22e6554b57
MaxSeiringer <seiringer.maximilian@hak-liezen.at>
2024-01-13 20:36:24 +0100

    .env

--- firebaseKeys/configKey.mjs
+++ firebaseKeys/configKey.mjs
@@ -1,11 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
-    apiKey: "AIzaSyDpNbXF-hAcUyY2vvJoux9sk6w5BE3WIFs",
-    authDomain: "lightweight-58f7b.firebaseapp.com",
-    databaseURL: "https://lightweight-58f7b-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "lightweight-58f7b",
-    storageBucket: "lightweight-58f7b.appspot.com",
-    messagingSenderId: "104798889393",
-    appId: "1:104798889393:web:85858e4c2af9ac24d9c990",
-    measurementId: "G-7NN2FF6MET"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
   };
 export default config;
