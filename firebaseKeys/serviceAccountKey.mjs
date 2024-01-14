3972c75e9235cdafd7406abfb18cbca82992e9a1
MaxSeiringer <seiringer.maximilian@hak-liezen.at>
2024-01-14 10:43:58 +0100

    Update serviceAccountKey.mjs

--- firebaseKeys/serviceAccountKey.mjs
+++ firebaseKeys/serviceAccountKey.mjs
@@ -5,7 +5,7 @@ const serviceAccount = {
     "type": process.env.type,
     "project_id": process.env.project_id,
     "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key,
+    "private_key": "-----BEGIN PRIVATE KEY-----\n"+ process.env.private_key1 + "\n" + process.env.private_key2 +"\n"+ process.env.private_key3 +"\n"+ process.env.private_key4 +"\n"+ process.env.private_key5 +"\n"+ process.env.private_key6 +"\n"+ process.env.private_key6 +"\n"+ process.env.private_key7 +"\n"+ process.env.private_key8 +"\n"+ process.env.private_key9 +"\n"+ process.env.private_key10 +"\n"+ process.env.private_key11 +"\n"+ process.env.private_key12 +"\n"+ process.env.private_key13 +"\n"+ process.env.private_key14 +"\n"+ process.env.private_key15 +"\n"+ process.env.private_key16 +"\n"+ process.env.private_key17 +"\n"+ process.env.private_key18 +"\n"+ process.env.private_key19 +"\n"+ process.env.private_key20 +"\n"+ process.env.private_key21 +"\n"+ process.env.private_key22 +"\n"+ process.env.private_key23 +"\n"+ process.env.private_key24 +"\n"+ process.env.private_key25 + "\n"+ process.env.private_key26 +"\n-----END PRIVATE KEY-----\n",
     "client_email": process.env.client_email,
     "client_id": process.env.client_id,
     "auth_uri": process.env.auth_uri,
