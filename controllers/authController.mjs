6ee2dd51946994ce2803fba899f7cc1698825b64
MaxSeiringer <seiringer.maximilian@hak-liezen.at>
2024-02-17 20:06:27 +0100

    Update authController.mjs

--- controllers/authController.mjs
+++ controllers/authController.mjs
@@ -16,7 +16,7 @@ const generateToken = (uid) => {
 };
 
 
-//Registrieren von Usern
+// Registrieren von Usern
 const register = async (req, res) => {
   const { email, password } = req.body;
 
@@ -35,7 +35,7 @@ const register = async (req, res) => {
         'height': 0
       });
 
-      // Basic_calories wird beim erstellen des accounts bereits erstellt in users -> uid -> nutrition -> food_values
+      // Basic_calories wird beim Erstellen des Accounts bereits erstellt in users -> uid -> nutrition -> food_values
       await firebase.firestore().collection('users').doc(uid).collection('nutrition').doc('food_values').set({
         'basic_calories': 0
       });
@@ -45,41 +45,65 @@ const register = async (req, res) => {
       console.log('Documents created');
       res.json({ uid, token, success: true });
     } catch (e) {
-      console.error(e);
-      res.status(500).json({ message: 'Error creating documents', success: false });
+      console.error('Error creating documents:', e);
+
+      // Spezifische Fehler behandeln und entsprechende HTTP-Statuscodes und Fehlermeldungen zurückgeben
+      if (e instanceof SomeSpecificError) {
+        res.status(400).json({ message: 'Some specific error occurred', success: false });
+      } else {
+        res.status(500).json({ message: 'Error creating documents', success: false });
+      }
     }
   } catch (error) {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    res.status(401).json({ message: `Registration error: ${errorCode} - ${errorMessage}`, success: false });
+    console.error('Registration error:', error);
+
+    // Spezifische Fehler behandeln und entsprechende HTTP-Statuscodes und Fehlermeldungen zurückgeben
+    if (error.code === 'auth/weak-password') {
+      res.status(400).json({ message: 'Weak password', success: false });
+    } else if (error.code === 'auth/email-already-in-use') {
+      res.status(400).json({ message: 'Email already in use', success: false });
+    } else if (error.code === 'auth/invalid-email') {
+        res.status(400).json({ message: 'Invalid email format. Please provide a valid email address.', success: false });
+    } else {
+      res.status(401).json({ message: `Registration error: ${error.code} - ${error.message}`, success: false });
+    }
   }
 };
 
 
-//Anmelden von User
+
+// Anmelden von User
 const login = async (req, res) => {
   const { email, password } = req.body;
 
-  //Ausgabe der Anfrage in der Console
-  console.log('Request body:', req.body);
-
   try {
-    //Firebase funktion überprüft übergebene Email und Passwort
+    // Firebase-Funktion überprüft übergebene Email und Passwort
     const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
     const user = userCredential.user;
     const uid = user.uid;
 
     const token = await generateToken(uid, email);
 
-    //Übergabe der UID an das Frontend
+    // Übergabe der UID an das Frontend
     res.json({ uid, token, success: true });
   } catch (error) {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.error(errorCode, errorMessage);
-    res.status(401).json({ message: `Login failed: ${errorCode} - ${errorMessage}`, success: false });
+    console.error('Login error:', error);
+
+    // Spezifische Fehler behandeln und entsprechende HTTP-Statuscodes und Fehlermeldungen zurückgeben
+    if (error.code === 'auth/user-not-found') {
+      res.status(404).json({ message: 'User not found. Please check your email or register.', success: false });
+    } else if (error.code === 'auth/wrong-password') {
+      res.status(401).json({ message: 'Incorrect password. Please try again.', success: false });
+    } else if (error.code === 'auth/invalid-email') {
+      res.status(400).json({ message: 'Invalid email format. Please provide a valid email address.', success: false });
+    } else if (error.code === 'auth/user-disabled') {
+      res.status(401).json({ message: 'User account is disabled. Please contact support.', success: false });
+    } else {
+      res.status(401).json({ message: `Login failed: ${error.code} - ${error.message}`, success: false });
+    }
   }
 };
+
 
 
 //Passwort zurücksetzen nicht im code eingebaut.
