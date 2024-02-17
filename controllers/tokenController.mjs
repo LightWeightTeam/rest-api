01581e74740f7d79e7f65bec00202ccdd84448ca
MaxSeiringer <seiringer.maximilian@hak-liezen.at>
2024-02-17 12:12:14 +0100

    Update tokenController.mjs

--- controllers/tokenController.mjs
+++ controllers/tokenController.mjs
@@ -1,6 +1,7 @@
 import admin from 'firebase-admin';
 const adminApp = admin.app();
 
+// Middleware zur Token-Authentifizierung
 const authenticateToken = async (req, res, next) => {
   const token = req.headers.authorization;
 
@@ -9,12 +10,24 @@ const authenticateToken = async (req, res, next) => {
   }
 
   try {
+    // Token überprüfen und dekodieren
     const decodedToken = await admin.auth().verifyIdToken(token);
     const { uid, email } = decodedToken;
 
-    // Benutzerinformationen für weitere Verwendung im Request speichern
-    req.user = { uid, email };
-    next();
+    // Überprüfen, ob UID und E-Mail in Firebase Auth übereinstimmen
+    const userRecord = await admin.auth().getUser(uid);
+
+    if (userRecord.email === email) {
+      // Benutzerinformationen aus dem Token extrahieren
+      req.user = { uid, email };
+
+      // Hier kannst du weitere Überprüfungen durchführen, wenn nötig
+
+      // Weiter zum nächsten Middleware oder zur Route
+      next();
+    } else {
+      return res.status(403).json({ message: 'Forbidden: UID and Email do not match', success: false });
+    }
   } catch (err) {
     return res.status(403).json({ message: 'Forbidden: Invalid token', success: false });
   }
