98077affb744e2ca3272d331bf5fe2c3799329e7
MaxSeiringer <seiringer.maximilian@hak-liezen.at>
2024-02-17 11:58:47 +0100

    Update tokenController.mjs

--- controllers/tokenController.mjs
+++ controllers/tokenController.mjs
@@ -1,7 +1,6 @@
 import admin from 'firebase-admin';
 const adminApp = admin.app();
 
-
 const authenticateToken = async (req, res, next) => {
   const token = req.headers.authorization;
 
@@ -12,14 +11,6 @@ const authenticateToken = async (req, res, next) => {
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
     const { uid, email } = decodedToken;
-    console.log('Decoded Token:', decodedToken);
-console.log('req.query.uid:', req.query.uid);
-console.log('req.query.email:', req.query.email);
-
-    // Überprüfen, ob UID und E-Mail aus dem Token mit der Anfrage übereinstimmen
-    if (uid !== req.query.uid || email !== req.query.email) {
-      return res.status(403).json({ message: 'Forbidden: Token does not match user', success: false });
-    }
 
     // Benutzerinformationen für weitere Verwendung im Request speichern
     req.user = { uid, email };
@@ -30,4 +21,3 @@ console.log('req.query.email:', req.query.email);
 };
 
 export { authenticateToken };
-
